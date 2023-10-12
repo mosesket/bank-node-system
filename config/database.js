054f6a51f47a299f64fe4df9706879a639a1da86
MosesKet <sirketxp@gmail.com>
2023-10-12 10:08:31 +0100

    added database

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongoose");
+  } catch (error) {
+    console.log("Cannot connect to Mongoose");
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Database Disconnected");
+});
+
+module.exports = {
+  connectToDatabase
+};
